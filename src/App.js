ec66ca1e41c97d6d128288a4fe14ffa29791939f
Saphyel <Saphyel@gmail.com>
2018-08-03 20:54:06 +0100

    Add sw.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ChocoboNew from './components/ChocoboNew';
 import ChocoboList from './components/ChocoboList';
 
 const App = () =>
-  <div id="main" className="container-fluid">
+  <div id="main" className="container-fluid text-center">
     <h1>ChocoGlaive</h1>
     <p>Get the score of the chocobos of your stable.
     So you will release your weakest chocobo.</p>
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -7,17 +7,41 @@ class TableBody extends React.Component {
     await del(id);
     window.location.reload();
   }
+
+  calcSpeed(value) {
+    if (value === 'C') {
+      return 110;
+    }
+    else if (value === 'B') {
+      return 120;
+    }
+    else if (value === 'A') {
+      return 130;
+    }
+    else if (value === 'A+') {
+      return 140;
+    }
+    return 100;
+  }
+
+  calculator(chocobo) {
+    const hp = parseInt(chocobo.hp);
+    const attack = parseInt(chocobo.attack);
+    const speed = this.calcSpeed(chocobo.speed)
+    return (hp / 20) + (attack / 10) + speed;
+  }
+
   render() {
     return (
       <tbody id={this.props.name}>
         {this.props.list.map(opt => {
           return (
-            <tr key={opt.chocobo.hp}>
+            <tr key={opt.id}>
               <td>{opt.chocobo.hp}</td>
               <td>{opt.chocobo.attack}</td>
               <td>{opt.chocobo.speed}</td>
               <td>{opt.chocobo.colour}</td>
-              <td>{opt.chocobo.attack}</td>
+              <td>{this.calculator(opt.chocobo)}</td>
               <td>
                 <Button
                   pk={() => this.deleteItem(opt.id)}
