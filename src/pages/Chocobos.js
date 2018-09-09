54dc7fee34a86aa2d58878bc849e47addda88d2b
Carlos Jimenez <Saphyel@gmail.com>
2018-09-09 17:08:55 +0100

    Improve Chocobo page (#7)
    * migrate training into chocobos page
    
    * migrate stable into chocobos page

--- src/pages/Chocobos.js
+++ src/pages/Chocobos.js
@@ -1,24 +1,101 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import ContentTable from '../components/ContentTable';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Stable from './Stable';
-import Training from './Training';
+import ChocoboList from '../components/ChocoboList';
+import {TrainingService} from '../services/TrainingService';
+import {get, keys, set, del} from "idb-keyval";
+
+const uuidv1 = require('uuid/v1');
+
+function Stable(props) {
+  return (
+    <Grid container spacing={8} alignItems="stretch" direction="column">
+      <ChocoboList
+        DeleteItem={props.delete}
+        addNewItem={props.add}
+        header={['HP', 'Attack', 'Speed', 'Colour', 'Value', 'Manage']}
+        items={props.children}
+      />
+    </Grid>
+  );
+}
+
+function Training(props) {
+  return (
+    <Grid container spacing={8} alignItems="stretch" justify="center">
+      {props.children.map(train =>
+        <ContentTable
+          key={train.id}
+          title={train.title}
+          headers={train.headers}
+          contents={train.contents}
+        />
+      )}
+    </Grid>
+  );
+}
 
 class Chocobos extends React.Component {
+
   constructor() {
     super();
     this.state = {
       value: 'stable',
+      stable: [],
+      training: []
     };
   }
 
-  handleChange = (event, value) => {
-    this.setState({ value });
+  async getItems() {
+    let payload = [];
+    const Ids = await keys();
+    for (var i = 0; i < Ids.length; i++) {
+      payload.push({id: Ids[i], chocobo: await get(Ids[i])})
+    }
+    return {
+      stable: payload,
+      training: await TrainingService.fetchAll()
+    };
+  }
+
+  async componentDidMount() {
+    this.setState(await this.getItems());
+  }
+
+  addNewItem = (hp, attack, colour, speed) => {
+    const payload = {
+      hp: hp,
+      attack: attack,
+      speed: speed,
+      colour: colour
+    };
+
+    let id = uuidv1();
+    let item = {id: id, chocobo: payload};
+    set(id, payload);
+
+    this.setState(prevState => {
+      prevState.stable.push(item);
+      return {stable: prevState.stable};
+    });
   };
 
+  DeleteItem = (id) => {
+    del(id);
+
+    this.setState(prevState => {
+      prevState.stable = prevState.stable.filter(chocobo => chocobo.id !== id);
+      return {stable: prevState.stable};
+    });
+  };
+
+  handleChange = (event, value) => this.setState({value});
+
   render() {
-    const {value} = this.state;
+    const {value, stable, training} = this.state;
 
     return (
       <main>
@@ -28,8 +105,8 @@ class Chocobos extends React.Component {
             <Tab value="training" label="Training" />
           </Tabs>
         </AppBar>
-        {value === 'stable' && <Stable/>}
-        {value === 'training' && <Training/>}
+        {value === 'stable' && <Stable delete={this.DeleteItem} add={this.addNewItem}>{stable}</Stable>}
+        {value === 'training' && <Training>{training}</Training>}
       </main>
     );
   }
