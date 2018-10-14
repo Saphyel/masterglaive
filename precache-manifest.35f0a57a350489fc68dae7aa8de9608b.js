a0fb2aa2cc3feacf3ee2d75615cc1db968997ece
Saphyel <Saphyel@gmail.com>
2018-10-14 16:50:23 +0100

    Updates

---
+++ precache-manifest.35f0a57a350489fc68dae7aa8de9608b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "791d6debc5c27fc1232b",
+    "url": "/masterglaive/static/js/runtime~main.791d6deb.js"
+  },
+  {
+    "revision": "916fa7eee611e1de8533",
+    "url": "/masterglaive/static/js/main.916fa7ee.chunk.js"
+  },
+  {
+    "revision": "577338ebe75761d67f21",
+    "url": "/masterglaive/static/js/1.577338eb.chunk.js"
+  },
+  {
+    "revision": "9bc0e6b41b379385763aa76edf164124",
+    "url": "/masterglaive/index.html"
+  }
+];
